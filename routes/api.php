9467ed78a286f6eea3c48ba578273a0231397656
AmeliaaRaisaa <ameliaraisaarifien@gmail.com>
2026-03-18 09:37:25 +0700

    first-commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\TagController;
 
 Route::prefix('books')->group(function(){
 
@@ -20,3 +23,17 @@ Route::get(
 
 Route::get('/filterByQuery', 
 [BookController::class, 'filterBooksByQuery']);
+
+Route::prefix('posts')->group(function () {
+    Route::post('/', [PostController::class, 'createPost']);
+    Route::get('/{id}', [PostController::class, 'getPostById']);
+    Route::put('/{id}/tag/{tagId}', [PostController::class, 'addTag']);
+});
+
+Route::prefix('comments')->group(function () {
+    Route::post('/', [CommentController::class, 'createComment']);
+});
+
+Route::prefix('tags')->group(function () {
+    Route::post('/', [TagController::class, 'createTag']);
+});
